7ae0754308c03fadc172e6a295a39de1e4e6cdbb
Ayush <ayushshivhare51@gmail.com>
2025-04-01 14:26:43 +0530

    feat: text area and button init

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,10 +1,21 @@
-import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Create() {
   return (
     <>
-      <div>
-        <Input />
+      <div className="flex border border-black h-screen ">
+        <div className="p-2 border border-black w-[25%] space-y-5">
+          <h1>Generate Images</h1>
+          <Textarea
+            className="resize-none"
+            placeholder="Describe what you want to see"
+          />
+          <Button className="w-full" variant="default">
+            Generate
+          </Button>
+        </div>
+        <div className="border border-black w-full">Right</div>
       </div>
     </>
   );
