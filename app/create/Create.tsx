37a1fad59671707ca5f51db08fe7dc20c48c4118
Ayush <ayushshivhare51@gmail.com>
2025-04-13 13:54:21 +0530

    chore(style): updating create route and avtar with dropdown

--- app/create/Create.tsx
+++ app/create/Create.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Textarea } from "@/components/ui/textarea";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import {
@@ -17,6 +18,16 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
 import { ImageProp } from "./Right";
+import { useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Switch } from "@/components/ui/switch";
 
 const FormSchema = z.object({
   prompt: z
@@ -31,6 +42,9 @@ const FormSchema = z.object({
 
 export default function Create() {
   const [image, setImage] = useState<ImageProp[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const session = useSession();
 
   const fetchImages = useCallback(async () => {
     const response = await axios.get("/api/getImages");
@@ -46,17 +60,25 @@ export default function Create() {
   });
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
-    const response = await axios.post("/api/generate", {
-      prompt: data.prompt,
-    });
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/generate", {
+        prompt: data.prompt,
+      });
 
-    if (response.data.success) {
-      const newImage = {
-        id: response.data.image.id,
-        url: response.data.image.url,
-      };
-      setImage((prev) => [newImage, ...prev]);
-      form.reset();
+      if (response.data.success) {
+        const newImage = {
+          id: response.data.image.id,
+          url: response.data.image.url,
+        };
+        setImage((prev) => [newImage, ...prev]);
+        form.reset();
+      }
+    } catch (error) {
+      console.log(error);
+      alert("An error occurred while generating the image.");
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -73,6 +95,7 @@ export default function Create() {
                   <div className="flex gap-2 justify-center items-end">
                     <FormControl>
                       <Textarea
+                        disabled={loading}
                         className={`resize-none rounded-none ${poppins.className}`}
                         placeholder="Describe what you want to see!"
                         {...field}
@@ -80,6 +103,7 @@ export default function Create() {
                     </FormControl>
                     <div className="pb-2">
                       <button
+                        disabled={loading}
                         type="submit"
                         className={`bg-[#c0c0c0] text-xl border border-white text-black hover:font-semibold cursor-pointer h-10 w-40`}
                       >
@@ -103,28 +127,48 @@ export default function Create() {
 
   return (
     <>
-      <div className="flex min-h-screen w-full overflow-hidden">
-        {/* Left */}
-        <div className="min-w-[25%] max-w-[25%] border border-white"></div>
-
-        {/* Right */}
-        <div className="min-w-[75%] max-w-[75%] w-full p-4 space-y-5">
+      <div className="flex overflow-hidden">
+        <div className="flex justify-between items-center">
           <h1 className="text-5xl">AI IMAGE CREATION</h1>
-          <div>{formRender()}</div>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar className="w-12 h-12">
+                <AvatarImage src={`${session.data?.user?.image}`} />
+                <AvatarFallback className="text-black">
+                  {session.data?.user?.name
+                    ?.split(" ")[0]
+                    .charAt(0)
+                    .toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer">
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
 
-          <h1 className="text-2xl">Images</h1>
-          <div className="grid grid-cols-3 w-[85%] gap-5 p-4">
-            {image.map((img) => (
-              <Image
-                key={img.id}
-                src={img.url}
-                width={500}
-                height={500}
-                className="rounded-xl"
-                alt="Image"
-              />
-            ))}
-          </div>
+        <div>{formRender()}</div>
+
+        <h1 className="text-2xl">Images</h1>
+        <div className="grid grid-cols-3 w-[85%] gap-5 p-4">
+          {image.map((img) => (
+            <Image
+              key={img.id}
+              src={img.url}
+              width={500}
+              height={500}
+              className="rounded-xl"
+              alt="Image"
+            />
+          ))}
         </div>
       </div>
     </>
