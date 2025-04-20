7693790bbae2c350e7869969cf88484ce33d6ef5
Ayush <ayushshivhare51@gmail.com>
2025-04-20 11:51:51 +0530

    chore: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from '@/components/ui/button';
 import Navbar from '@/components/Navbar';
 
 export default async function Page() {
   return (
     <>
       <Navbar />
-      <h1>Platform Gallery </h1>
+      <h1>Creativity, Unleashed. </h1>
+      <p>
+        Leverage generative AI with a unique suite of tools to convey your ideas
+        to the world.
+      </p>
+      <Button variant={'secondary'}>Get started</Button>
     </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,42 @@
+import Link from 'next/link';
 import Login from '../components/home/Login';
 
 export default function Navbar() {
+  const navbar = [
+    {
+      name: 'Home',
+      href: '/',
+    },
+    {
+      name: 'Gallery',
+      href: '#',
+    },
+    {
+      name: 'About',
+      href: '#',
+    },
+    {
+      name: 'Contact',
+      href: '#',
+    },
+  ];
   return (
     <>
-      <nav className='flex items-center justify-between p-4'>
+      <nav className='flex items-center justify-between border-b-2 border-gray-300 p-3 px-20'>
         <h1 className='text-2xl lg:text-5xl'>PhotoAI</h1>
 
         <div>
           <ul className='flex space-x-4'>
-            <li>
-              <a href='/'>Home</a>
-            </li>
-            <li>
-              <a href='/create'>Create</a>
-            </li>
-            <li>
-              <a href='/gallery'>Gallery</a>
-            </li>
+            {navbar.map((item) => (
+              <li key={item.name}>
+                <Link
+                  href={item.href}
+                  className='text-lg hover:text-blue-500 lg:text-xl'
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
